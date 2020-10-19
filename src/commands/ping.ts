6439d6d18e6ceaeda631afd896be17bd326f12b1
Daniel Gimenes <daniel@danielgimenes.com.br>
2020-10-19 00:24:57 -0300

    Remove arquivos não usados

--- src/commands/ping.ts
+++
@@ -1,7 +0,0 @@
-import { Client, Message } from 'discord.js';
-
-export const pingCommand = (client: Client, message: Message) => {
-  console.log('pingCommand');
-
-  return message.reply('pong');
-};
